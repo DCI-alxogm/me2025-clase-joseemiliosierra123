9625cad06d80351186c5dbd982f1da867c7c23dc
joseemiliosierra123 <joseemiliosmem3@gmail.com>
2025-11-05 16:50:10 -0600

    Add files via upload

---
+++ Semana 14/Nov_5_Avance_metodo_gradiente.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x0, x1, y0, y1, f0, f1, dfdx, dfdy, h, g;
+    
+    x0 = -1;
+    y0 = 1;
+    
+    dfdx = 2*y0 + 2 - 2*x0;
+    dfdy = 2*x0 - 4*y0;
+    
+    x1 = x0 + 
+    
+    f1 = 2*x1*y1 + 2*x1 - pow(x1,2) - 2*pow(y1,2);
+    
+    g = f1;
+    
+}
